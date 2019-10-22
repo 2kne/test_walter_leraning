6d75443668d99e72bc9aeb44a82a81bce23eb251
Guiraudie <louis.guiraudie@epitech.eu>
2019-10-22 17:12:11 +0200

    bouton start/pause ok

--- walterlearning/src/App.js
+++ walterlearning/src/App.js
@@ -4,22 +4,25 @@ import './App.css';
 
 class App extends React.Component {
     state = {
-        left : 4,
-        right : 0,
-        background_col : "blue",
-        off_color: false
+        btn_start: false,
+        btn_pause: false,
+        left: 4,
+        right: 0,
+        background_col: "blue",
+        off_color: false,
+        pause : "Pause",
+        first_start : true,
     };
 
-
-
     setcompteur = (left, right) => {
-        if (right+left !== 0) {
+        if (right + left !== 0) {
             if (right === 0) {
-                this.setState({ left : left-1,
-                                     right : right+9})
-            }
-            else {
-                this.setState({right : right-1})
+                this.setState({
+                    left: left - 1,
+                    right: right + 9
+                })
+            } else {
+                this.setState({right: right - 1})
             }
         }
     };
@@ -28,22 +31,49 @@ class App extends React.Component {
         const {left} = this.state;
         const {right} = this.state;
         if (left === 1 && right === 2) {
-            this.setState({background_col:'red',
-                                off_color: true});
-        }
-        else if (left+right < 3) {
-            this.setState({background_col:'orange'})
+            this.setState({
+                background_col: 'red',
+                off_color: true
+            });
+        } else if (left + right < 3) {
+            this.setState({background_col: 'orange'})
         }
 
     };
 
     start = () => {
-        setInterval(() => {
-          this.setcompteur(this.state.left, this.state.right);
-          if (this.state.off_color === false) {
-              this.change_color();
-          }
-        },1000)
+        this.setState({first_start : false});
+        if (this.state.btn_start === false) {
+            this.timer = setInterval(() => {
+                this.setcompteur(this.state.left, this.state.right);
+                if (this.state.off_color === false) {
+                    this.change_color();
+                }
+            }, 1000)
+            this.setState({btn_start: true});
+        } else if (this.state.btn_pause === true) {
+            clearInterval(this.timer);
+        }
+
+    };
+
+    pause = () => {
+        if (this.state.first_start === false) {
+            if (this.state.btn_pause === false) {
+                this.setState({btn_start: true,
+                    btn_pause: true,
+                    pause : "Play"}, () => {
+                    this.start();
+                });
+            }
+            else if (this.state.btn_pause === true) {
+                this.setState({btn_start: false,
+                    btn_pause: false,
+                    pause : "Pause"}, () => {
+                    this.start();
+                });
+            }
+        }
     };
 
 
@@ -51,17 +81,21 @@ class App extends React.Component {
         const {left} = this.state;
         const {right} = this.state;
         const {background_col} = this.state;
+        const {pause} = this.state;
 
         return (
-                <div className='content'>
-                    <div style={{ background: background_col, width: '100px', height: '100px' }}>
-                        {left}
-                        {right}
-                    </div>
-                    <button onClick={() => this.start()}>Start</button>
+            <div className='content'>
+                <div style={{background: background_col, width: '100px', height: '100px'}}>
+                    {left}
+                    {right}
                 </div>
+                <div className="btn-control">
+                    <button onClick={() => this.start()}>Start</button>
+                    <button onClick={() => this.pause()}>{pause}</button>
+                </div>
+            </div>
         );
     }
 }
 
-    export default App;
+export default App;
